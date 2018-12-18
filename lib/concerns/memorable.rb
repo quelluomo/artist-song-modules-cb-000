75ef5f4ace7b3ea8973052e4a10c4afc5b97e055
Matteo Muscas <thatman.j@gmail.com>
2018-12-18 10:50:49 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,18 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def reset_all
-    all.clear
+    def count
+      self.all.count
+    end
   end
 
-  def count
-    all.count
-  end
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
+  end 
 
 end
